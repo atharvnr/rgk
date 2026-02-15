c72691870be4e600f1cf70737e9a6cb413f8ae1a
atharv <atharv.nithinravanikar@gmail.com>
2026-02-14 19:33:07 -0800

    bf: auth0 init

--- frontporch/app/_layout.tsx
+++ frontporch/app/_layout.tsx
@@ -1,14 +1,40 @@
+import { useEffect } from "react";
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import { store } from "../src/store";
 import { theme } from "../src/theme";
+import { setToken, setLoading } from "../src/store/authSlice";
+import { getStoredToken } from "../src/services/auth";
+
+function AppInitializer({ children }: { children: React.ReactNode }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = await getStoredToken();
+        if (token) {
+          dispatch(setToken(token));
+        } else {
+          dispatch(setLoading(false));
+        }
+      } catch {
+        dispatch(setLoading(false));
+      }
+    })();
+  }, [dispatch]);
+
+  return <>{children}</>;
+}
 
 export default function RootLayout() {
   return (
     <Provider store={store}>
       <PaperProvider theme={theme}>
-        <Stack screenOptions={{ headerShown: false }} />
+        <AppInitializer>
+          <Stack screenOptions={{ headerShown: false }} />
+        </AppInitializer>
       </PaperProvider>
     </Provider>
   );
